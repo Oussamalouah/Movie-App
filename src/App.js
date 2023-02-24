48c58ea327d85b373d0c05e393ed684d5d417b9a
oussamalouah <oussamalouah65@gmail.com>
2023-02-24 12:05:08 +0100

    movie-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState,useEffect } from 'react';
+import MovieCard from './MovieCard'
+
+import './App.css';
+import SearchIcon from './searchIcon.png';
+
+const API_URL ='http://www.omdbapi.com?apikey=d9b2dac3'
+const App = () =>{
+            
+  const [movies, setMovies] = useState([]);
+  const [searchhamid,setsearchhamid] = useState([])
+    const searchMovies = async(title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    }
+
+    useEffect(() =>{
+        searchMovies('Spiderman');
+    },[]);
+    return (
+        <div className ="app">
+           <h1>Oussama's Movies</h1>
+           <div className="search">
+           <input
+           placehodler ="Search for movies"
+           value={searchhamid}
+           onChange={(e) => setsearchhamid(e.target.value)}/>
+           <img
+             src={SearchIcon}
+             alt="SearchIcon"
+             onClick={()=> searchMovies(searchhamid)}/>
+           </div>
+           
+           {
+            movies?.length > 0
+             ? (
+                <div className="container">
+               {movies.map((movie)=>(
+                <MovieCard movie = {movie}/>
+               ))}
+                </div>
+               ): (
+                    <div className='empty'>
+                        <h2>No Movies Found</h2>
+                      </div>  
+               )
+           }
+        </div>
+        
+    );
 }
 
-export default App;
+
+export default  App;
